15ad443442c5440a0e11da07bc9a2c56e282df6b
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2024-11-27 02:08:21 -0600

    Nix flake functionality

---
+++ .dotfiles/snowy/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A Nix-flake-based development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ocaml ocamlformat cargo clang lldb gdb neovim bun ] ++
+            (with pkgs.ocamlPackages; [ dune_3 odoc utop ]);
+        };
+      });
+    };
+}
