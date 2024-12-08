d3dfa49e2ff592c9b96ddd3d9118526f91812159
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2024-12-07 18:40:34 -0600

    update snowy

--- .dotfiles/snowy/flake.nix
+++ .dotfiles/snowy/flake.nix
@@ -1,13 +1,14 @@
 {
-  description = "A Nix-flake-based development environment";
+  description = "A Nix-flake-based development environment intended for deployment on a silverblue-based envrionment.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixvim.url = "github:nix-community/nixvim"
   };
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
@@ -15,9 +16,10 @@
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ ocaml ocamlformat cargo clang lldb gdb neovim bun micro biome swc zoxide eza bat ] ++
-            (with pkgs.ocamlPackages; [ dune_3 odoc utop ocaml-lsp ]);
-          shellHook = ''
+          packages = with pkgs; [ ocaml ocamlformat cargo clang lldb gdb bun micro biome swc zoxide eza bat ] ++
+            (with pkgs.ocamlPackages; [ dune_3 odoc utop ocaml-lsp ])
+            ++ (with pkgs.vimPlugins; [  ] );
+        shellHook = ''
 	    unalias micro &> /dev/null
 	    export FLAKE=true
             export EDITOR=micro
