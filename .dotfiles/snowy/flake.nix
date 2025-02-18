8d64a5233b26246fcdb69b1f953ad47734d4d1dd
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-02-18 17:06:11 -0600

    fixed?

--- .dotfiles/snowy/flake.nix
+++ .dotfiles/snowy/flake.nix
@@ -2,14 +2,26 @@
   description = "A Nix-flake-based development environment intended for deployment on a silverblue-based envrionment.";
 
   inputs = {
+    codine = {
+      url = "path:./codine";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nuka.url = "path:./nuka/";
   };
 
-  outputs = { self, nixpkgs, nuka, ... }: let
+  outputs = {
+    self,
+    codine,
+    nixpkgs,
+    nuka,
+    ...
+  }: let
     supportedSystems = [
       "x86_64-linux"
-      # "aarch64-linux"
+      "aarch64-linux"
     ];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
@@ -17,30 +29,25 @@
           pkgs = import nixpkgs {inherit system;};
         });
   in {
-    devShells = forEachSupportedSystem ({pkgs}:
-    let
-      default-packages = with pkgs; [ bat eza fd rust-parallel zoxide ];
-      ocaml-packages = with pkgs; [ ocaml ocamlformat ] ++ (with pkgs.ocamlPackages; [ dune_3 odoc utop ocaml-lsp ]);
-      rust-packages = with pkgs; [ cargo ];
-      js-packages = with pkgs; [ bun biome ];
-      c-packages = with pkgs; [ gdb ];
-      vs-packages = with pkgs; [ vscodium ];
+    devShells = forEachSupportedSystem ({pkgs}: let
+      shell = shell-packages:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [bashInteractive];
+          packages = shell-packages;
+        };
+      default-packages = with pkgs; [bat eza fd rust-parallel zoxide];
+      ocaml-packages = with pkgs; [ocaml ocamlformat] ++ (with pkgs.ocamlPackages; [dune_3 odoc utop ocaml-lsp]);
+      rust-packages = with pkgs; [cargo];
+      js-packages = with pkgs; [bun biome];
+      nix-packages = with pkgs; [alejandra];
+      c-packages = with pkgs; [gdb];
+      vs-packages = with pkgs; [codine.packages.x86_64-linux.default];
     in {
-      default = pkgs.mkShell {
-        packages = default-packages ++ c-packages ++ js-packages ++ rust-packages ++ [ nuka.packages.x86_64-linux.default ];
-      };
-      full = pkgs.mkShell {
-        packages = default-packages ++ c-packages ++ js-packages ++ rust-packages ++ ocaml-packages ++ [ nuka.packages.x86_64-linux.default ];
-      };
-      basic = pkgs.mkShell {
-        packages = default-packages;
-      };
-      vscodium = pkgs.mkShell {
-        packages = default-packages ++ vs-packages ++ js-packages ++ rust-packages;
-      };
-      vscodium-ocaml = pkgs.mkShell {
-        packages = default-packages ++ vs-packages ++ js-packages ++ ocaml-packages ++ rust-packages;
-      };
+      basic = shell (default-packages);
+      default = shell (default-packages ++ c-packages ++ js-packages ++ rust-packages ++ [nuka.packages.x86_64-linux.default]);
+      full = shell (default-packages ++ c-packages ++ js-packages ++ nix-packages ++ rust-packages ++ ocaml-packages ++ [nuka.packages.x86_64-linux.default]);
+      vscodium = shell (default-packages ++ vs-packages ++ js-packages ++ nix-packages ++ rust-packages);
+      vscodium-ocaml = shell (default-packages ++ vs-packages ++ js-packages ++ nix-packages ++ ocaml-packages ++ rust-packages);
     });
   };
 }
