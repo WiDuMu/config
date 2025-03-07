0ab02fb0bc360010acb1c8acbe174ab45bd83ed5
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-03-07 13:45:28 -0600

    Add home-manager

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Home Manager configuration of aurora";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nuka.url = "github:widumu/config/?dir=.dotfiles/snowy/nuka";
+  };
+
+  outputs = { nixpkgs, home-manager, nuka, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."aurora" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix
+        {
+            home.packages = [
+        	    nuka.packages.${system}.default
+            ];
+        }
+        ];
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
