7fbe5955e0da65caee25392fd4ab57ad1fb80807
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-03-11 17:29:58 -0500

    add some stuff

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -18,9 +18,17 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.biome
+    pkgs.bun
+    pkgs.eza
+    pkgs.cargo
+    pkgs.fd
+    pkgs.gdb
+    pkgs.rr
+    pkgs.tokei
+    pkgs.vlang
+    pkgs.zig
+    pkgs.zoxide
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
