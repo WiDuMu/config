21b38120000dae40dfd184602ecc2078e4c04bb4
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-05-27 20:41:38 -0500

    add fish on nixOS

--- .dotfiles/flakes/snowy/systems/users.nix
+++ .dotfiles/flakes/snowy/systems/users.nix
@@ -6,6 +6,7 @@
     packages = with pkgs; [
       kdePackages.kate
     ];
+    shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9l0Em3K1Zcs0f1c0pw3ZovL4Vg0GhOUgfWLZPsNx/W aurora"
     ];
