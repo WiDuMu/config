afb676c823da20a9825350243a4dafae1ec92f60
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-05-29 01:03:02 -0500

    helix configuration

--- .dotfiles/snowy/home.nix
+++ .dotfiles/snowy/home.nix
@@ -165,6 +165,12 @@
     enable = true;
     settings = {
       theme = "base16_transparent";
+      editor.cursor-shape = {
+        insert = "bar";
+        normal = "block";
+        select = "underline";
+      };
+      editor.file-picker.hidden = false;
     };
     package = pkgs.evil-helix;
   };
