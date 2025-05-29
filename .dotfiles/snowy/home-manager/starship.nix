8d75e87624ecd3204f697e622c65c46547f92916
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-05-29 14:23:03 -0500

    reenable starship

--- .dotfiles/snowy/home-manager/starship.nix
+++ .dotfiles/snowy/home-manager/starship.nix
@@ -3,7 +3,7 @@
   programs.starship = {
     enable = true;
     # interactiveOnly = true;
-    # settings = pkgs.lib.importTOML ./starship.toml;
+    settings = pkgs.lib.importTOML ./starship.toml;
     enableZshIntegration = true;
     enableNushellIntegration = true;
     enableBashIntegration = true;
