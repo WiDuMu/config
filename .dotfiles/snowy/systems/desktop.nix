ba3a35e7df2f3aea7c41bd821773df93eb890f8f
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-05-30 14:24:07 -0500

    theming

--- .dotfiles/snowy/systems/desktop.nix
+++ .dotfiles/snowy/systems/desktop.nix
@@ -53,5 +53,5 @@
   programs.firefox.enable = true;
 
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-black.yaml";
 }
