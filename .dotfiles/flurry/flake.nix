bec643a5162c3bdb96f7843b8a2f04922600a595
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-06-01 16:19:26 -0500

    don't use nixvim

--- .dotfiles/flurry/flake.nix
+++ .dotfiles/flurry/flake.nix
@@ -14,9 +14,9 @@
     # Software inputs
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.flake-parts.follows = "flake-parts";
+    # nixvim.url = "github:nix-community/nixvim";
+    # nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    # nixvim.inputs.flake-parts.follows = "flake-parts";
   };
 
   # Wired using https://nixos-unified.org/autowiring.html
