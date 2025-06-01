3a5e2327eb37218c2cffc1d630f8c62a5c102b93
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-06-01 18:39:41 -0500

    more modules

---
+++ .dotfiles/flurry/modules/home/nushell.nix
@@ -0,0 +1,14 @@
+{
+  programs.nushell = {
+    enable = true;
+    settings = {
+      show_banner = false;
+      completions.external = {
+        enable = true;
+        max_results = 200;
+      };
+    };
+    shellAliases = {
+    };
+  };
+}
