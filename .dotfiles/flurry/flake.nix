4aba6d959d127d1a7158ff9f5b1e466cc50d0bbb
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-06-01 15:58:32 -0500

    formatting and fixing stupid settings.json

--- .dotfiles/flurry/flake.nix
+++ .dotfiles/flurry/flake.nix
@@ -1,5 +1,4 @@
 {
-<<<<<<< HEAD
   description = "A home-manager template providing useful tools & settings for Nix-based development";
 
   inputs = {
@@ -23,49 +22,8 @@
   # Wired using https://nixos-unified.org/autowiring.html
   outputs = inputs:
     inputs.nixos-unified.lib.mkFlake
-      { inherit inputs; root = ./.; };
-=======
-  description = "A very basic flake";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
-
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} (top @ {
-      config,
-      withSystem,
-      moduleWithSystem,
-      ...
-    }: {
-      imports = [
-        # Optional: use external flake logic, e.g.
-        # inputs.foo.flakeModules.default
-      ];
-      flake = {
-        # Put your original flake attributes here.
-      };
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: {
-        packages.default = pkgs.hello;
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [hello];
-        };
-      };
-    });
->>>>>>> a2526cfaf90278156a735a9214c8150cdedd39a2
+    {
+      inherit inputs;
+      root = ./.;
+    };
 }
