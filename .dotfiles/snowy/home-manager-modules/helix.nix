b3c2b761f0dbd5f3f1c6a09f465c36978d7616e5
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-08-05 04:11:38 -0500

    helix theme

--- .dotfiles/snowy/home-manager-modules/helix.nix
+++ .dotfiles/snowy/home-manager-modules/helix.nix
@@ -2,7 +2,7 @@
   programs.helix = {
     enable = true;
     settings = {
-      # theme = "base16_transparent";
+      theme = "base16_transparent";
       editor.cursor-shape = {
         insert = "bar";
         normal = "block";
