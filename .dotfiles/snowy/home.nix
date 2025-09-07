8502d11b6be1b3920396e700d67cb24aa18d9913
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-09-06 22:48:58 -0500

    unfortunately it's a little broken so comment it out rn

--- .dotfiles/snowy/home.nix
+++ .dotfiles/snowy/home.nix
@@ -88,8 +88,8 @@ in {
       (pkgs.writeShellScriptBin "bak" ''
         mv "$1" "$1.bak"
       '')
-      # Suspend after a given amount of time
-      (pkgs.writeScriptBin "suspend_timed" ./shared/scripts/suspend_timed)
+      # # Suspend after a given amount of time
+      # (pkgs.writeScriptBin "suspend_timed" ./shared/scripts/suspend_timed)
     ]
     ++ (with pkgs; [
       corefonts
