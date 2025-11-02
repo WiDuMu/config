52c3f10677b28b6d0f61384fced4b1cc41811814
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-11-01 23:41:11 -0500

    add nixos version

--- .dotfiles/snowy/nixos-modules/default.nix
+++ .dotfiles/snowy/nixos-modules/default.nix
@@ -6,6 +6,8 @@
     ../shared/nix.nix
   ];
 
+  system.stateVersion = "24.11"; # Did you read the comment?
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
