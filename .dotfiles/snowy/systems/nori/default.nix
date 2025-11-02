2eaa1f01e1fbcd4c4a745f47f33bb3c5b24ff37d
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-11-01 23:21:51 -0500

    add import for nori

--- .dotfiles/snowy/systems/nori/default.nix
+++ .dotfiles/snowy/systems/nori/default.nix
@@ -9,6 +9,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../nixos-modules/default.nix
   ];
 
   networking.hostName = "nori"; # Define your hostname.
