790e82ca9a352697ab2451eee3128cb74feae3b7
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-11-01 22:20:29 -0500

    add nvf changes

--- .dotfiles/snowy/home-manager-modules/nvf.nix
+++ .dotfiles/snowy/home-manager-modules/nvf.nix
@@ -1,11 +1,270 @@
 {
   programs.nvf = {
-     enable = true;
-     settings = {
-       vim.viAlias = true;
-       vim.lsp = {
-         enable = true;
-       };
-     };
+    enable = true;
+    settings.vim = {
+      viAlias = true;
+      vimAlias = false;
+      lineNumberMode = "number";
+
+      spellcheck = {
+        enable = true;
+        programmingWordlist.enable = false;
+      };
+
+      lsp = {
+        # This must be enabled for the language modules to hook into
+        # the LSP API.
+        enable = true;
+
+        formatOnSave = true;
+        lspkind.enable = false;
+        lightbulb.enable = true;
+        lspsaga.enable = false;
+        trouble.enable = true;
+        lspSignature.enable = false; # conflicts with blink in maximal
+        otter-nvim.enable = true;
+        nvim-docs-view.enable = true;
+
+        servers.nixd.settings.nil.nix.autoArchive = false;
+      };
+
+      debugger = {
+        nvim-dap = {
+          enable = true;
+          ui.enable = true;
+        };
+      };
+
+      # This section does not include a comprehensive list of available language modules.
+      # To list all available language module options, please visit the nvf manual.
+      languages = {
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+
+        # Languages that will be supported in default and maximal configurations.
+        nix.enable = true;
+        markdown.enable = true;
+
+        # Languages that are enabled in the maximal configuration.
+        bash.enable = true;
+        clang.enable = true;
+        css.enable = true;
+        html.enable = true;
+        sql.enable = true;
+        java.enable = true;
+        kotlin.enable = false;
+        ts.enable = true;
+        go.enable = false;
+        lua.enable = false;
+        zig.enable = false;
+        python.enable = true;
+        typst.enable = true;
+        rust = {
+          enable = true;
+          crates.enable = true;
+        };
+
+        # Language modules that are not as common.
+        assembly.enable = false;
+        astro.enable = false;
+        nu.enable = false;
+        csharp.enable = false;
+        julia.enable = false;
+        vala.enable = false;
+        scala.enable = false;
+        r.enable = false;
+        gleam.enable = false;
+        dart.enable = false;
+        ocaml.enable = false;
+        elixir.enable = false;
+        haskell.enable = false;
+        ruby.enable = false;
+        fsharp.enable = false;
+
+        tailwind.enable = false;
+        svelte.enable = false;
+
+        # Nim LSP is broken on Darwin and therefore
+        # should be disabled by default. Users may still enable
+        # `vim.languages.vim` to enable it, this does not restrict
+        # that.
+        # See: <https://github.com/PMunch/nimlsp/issues/178#issue-2128106096>
+        nim.enable = false;
+      };
+
+      visuals = {
+        nvim-scrollbar.enable = true;
+        nvim-web-devicons.enable = true;
+        nvim-cursorline.enable = true;
+        cinnamon-nvim.enable = true;
+        fidget-nvim.enable = true;
+
+        highlight-undo.enable = true;
+        indent-blankline.enable = true;
+
+        # Fun
+        cellular-automaton.enable = false;
+      };
+
+      statusline = {
+        lualine = {
+          enable = true;
+          theme = "catppuccin";
+        };
+      };
+
+      theme = {
+        enable = true;
+        name = "catppuccin";
+        style = "mocha";
+        transparent = true;
+      };
+
+      autopairs.nvim-autopairs.enable = true;
+
+      # nvf provides various autocomplete options. The tried and tested nvim-cmp
+      # is enabled in default package, because it does not trigger a build. We
+      # enable blink-cmp in maximal because it needs to build its rust fuzzy
+      # matcher library.
+      autocomplete = {
+        nvim-cmp.enable = true;
+        # blink-cmp.enable = isMaximal;
+      };
+
+      snippets.luasnip.enable = true;
+
+      filetree = {
+        neo-tree = {
+          enable = true;
+        };
+      };
+
+      tabline = {
+        nvimBufferline.enable = true;
+      };
+
+      treesitter.context.enable = true;
+
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
+
+      telescope.enable = true;
+
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+        gitsigns.codeActions.enable = false; # throws an annoying debug message
+        neogit.enable = true;
+      };
+
+      minimap = {
+        minimap-vim.enable = false;
+        codewindow.enable = true; # lighter, faster, and uses lua for configuration
+      };
+
+      dashboard = {
+        dashboard-nvim.enable = false;
+        alpha.enable = true;
+      };
+
+      notify = {
+        nvim-notify.enable = true;
+      };
+
+      projects = {
+        project-nvim.enable = true;
+      };
+
+      utility = {
+        ccc.enable = false;
+        vim-wakatime.enable = false;
+        diffview-nvim.enable = true;
+        yanky-nvim.enable = false;
+        icon-picker.enable = true;
+        surround.enable = true; # helps with surrounding text elements: https://github.com/kylechui/nvim-surround
+        leetcode-nvim.enable = true; # https://github.com/kawre/leetcode.nvim
+        multicursors.enable = true;
+        smart-splits.enable = true;
+        undotree.enable = true;
+        nvim-biscuits.enable = true; # puts hints at what block a end character is ending
+
+        motion = {
+          hop.enable = true;
+          leap.enable = true;
+          precognition.enable = true;
+        };
+        images = {
+          image-nvim.enable = false;
+          img-clip.enable = true; # Allows images to be pasted into notes in neovim
+        };
+      };
+
+      notes = {
+        obsidian.enable = false; # FIXME: neovim fails to build if obsidian is enabled
+        neorg.enable = false;
+        orgmode.enable = false;
+        mind-nvim.enable = false; # Cool but not right now
+        todo-comments.enable = true;
+      };
+
+      terminal = {
+        toggleterm = {
+          enable = true;
+          lazygit.enable = true;
+        };
+      };
+
+      ui = {
+        borders.enable = true;
+        noice.enable = false; # Maybe this fixes the unable to actually accept a question thing?
+        colorizer.enable = true;
+        modes-nvim.enable = false; # the theme looks terrible with catppuccin
+        illuminate.enable = true;
+        breadcrumbs = {
+          enable = true;
+          navbuddy.enable = true;
+        };
+        smartcolumn = {
+          enable = true;
+          setupOpts.custom_colorcolumn = {
+            # this is a freeform module, it's `buftype = int;` for configuring column position
+            nix = "110";
+            ruby = "120";
+            java = "130";
+            go = ["90" "130"];
+          };
+        };
+        fastaction.enable = true;
+      };
+
+      #assistant = {
+      #  chatgpt.enable = false;
+      #  copilot = {
+      #    enable = false;
+      #    cmp.enable = false;
+      #  };
+      #  codecompanion-nvim.enable = false;
+      #  avante-nvim.enable = false;
+      #};
+
+      session = {
+        nvim-session-manager.enable = false;
+      };
+
+      gestures = {
+        gesture-nvim.enable = false;
+      };
+
+      comments = {
+        comment-nvim.enable = true;
+      };
+
+      presence = {
+        neocord.enable = false;
+      };
+    };
   };
 }
--- README.md
+++ README.md
@@ -1,37 +1,59 @@
 # Config
+
 This is a set of dotfiles and nix configurations created for my personal use.
 
 ## Architecture
-This set of dotfiles is strange by both dotfiles and nix's standards. This is due to the multitude of functions used in this repository. These include:
-* A set of non-nix dotfiles.
-* A standalone nix home-manager configuration
-* A NixOS configuration for desktops
-* A NixOS configuration for servers
+
+This set of dotfiles is strange by both dotfiles and nix's standards. This is
+due to the multitude of functions used in this repository. These include:
+
+- A set of non-nix dotfiles.
+- A standalone nix home-manager configuration
+- A NixOS configuration for desktops
+- A NixOS configuration for servers
 
 ## Usage
+
 ### Initialization
-Clone this repo as a ***bare repository*** to your home directory.
+
+Clone this repo as a _**bare repository**_ to your home directory.
+
 ```bash
 git clone --bare git@github.com:WiDuMu/config.git ~/.cfg
 ```
+
 Then checkout the head:
+
 ```bash
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 ```
-This contains a set of shell scripts in `~/.dotfiles/.bashrc.d`. If you do not use home-manager, source these in your `~/.bashrc`. Restart any `bash` shells and you should be able to use the alias `cfg` for git when working with config. This includes `cfg add`, `cfg commit`, etc, etc.
 
-### Home-manager intialization
-Install nix however do, with flakes enabled. Then run:
+This contains a set of shell scripts in `~/.dotfiles/.bashrc.d`. If you do not
+use home-manager, source these in your `~/.bashrc`. Restart any `bash` shells
+and you should be able to use the alias `cfg` for git when working with config.
+This includes `cfg add`, `cfg commit`, etc, etc.
+
+### Home-manager initialization
+
+Install nix however you want to, with flakes enabled. Then run:
+
 ```
 nix run home-manager switch
 ```
-This repo should have the requisite symlinks to allow it to build with just that command
+
+This repo should have the requisite symlinks to allow it to build with just that
+command
 
 ### NixOS
-Set hostname to the desired hostname in the build setup. Link the `snowy` directory containing `flake.nix` to `/etc/nixos/`. Then run:
+
+Set hostname to the desired hostname in the build setup. Link the `snowy`
+directory containing `flake.nix` to `/etc/nixos/`. Then run:
+
 ```bash
 sudo nixos-rebuild switch
 ```
 
 ### Silverblue
-To apply Nix to a silverblue based distro post-f42, follow the following guide: (https://gist.github.com/queeup/1666bc0a5558464817494037d612f094)
+
+To apply Nix to a silverblue based distro post-f42, follow the following guide:
+(https://gist.github.com/queeup/1666bc0a5558464817494037d612f094)
